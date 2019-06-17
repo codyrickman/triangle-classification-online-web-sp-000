c3439dbefb77180e611506af51759309dd84a21f
Cody Rickman <cody@creativemindstudios.com>
2019-06-17 01:44:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   def kind
     sum = l1 + l2
-    if (sum < l3 || l1 === 0 || l2 === 0 || l2 ===0)
+    if (sum < l3 || l1 === 0 || l2 === 0 || l2 ===0 || l1 < 0 || l2 < 0 || l3 < 0)
       begin
         raise TriangleError => error
       rescue TriangleError
