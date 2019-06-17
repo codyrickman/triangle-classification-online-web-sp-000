49c17e0725520807b764615df7f44e93c6546d79
Cody Rickman <cody@creativemindstudios.com>
2019-06-17 01:55:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     sum = l1 + l2
     sum2 = l2 + l3
     sum3 = l3 + l1
-    if (sum < l3 || sum2 < l1 || sum3 < l2 || l1 === 0 || l2 === 0 || l2 ===0 || l1 < 0 || l2 < 0 || l3 < 0)
+    if (sum > l3 || sum2 > l1 || sum3 > l2 || l1 === 0 || l2 === 0 || l2 ===0 || l1 < 0 || l2 < 0 || l3 < 0)
         raise TriangleError
     else
       if (l1 === l2 && l1 === l3 && l3 === l2)
