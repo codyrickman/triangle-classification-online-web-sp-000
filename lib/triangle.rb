77d66c7d42559900e7bc43f99f7cc30b97d18604
Cody Rickman <cody@creativemindstudios.com>
2019-06-17 01:46:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   def kind
     sum = l1 + l2
     if (sum < l3 || l1 === 0 || l2 === 0 || l2 ===0 || l1 < 0 || l2 < 0 || l3 < 0)=
-        raise TriangleError => error
+        raise TriangleError
     else
       if (l1 === l2 && l1 === l3 && l3 === l2)
         return :equilateral
